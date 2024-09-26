585a90275a4a9e55d99fc6cab060c0cbf6d59031
amat3 <121510770+amat3@users.noreply.github.com>
2024-09-26 11:56:57 +0200

    create post element endpoint

--- app/(routes)/(home)/components/FormAddElement/FormAddElement.tsx
+++ app/(routes)/(home)/components/FormAddElement/FormAddElement.tsx
@@ -3,6 +3,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import axios from "axios";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -29,10 +30,12 @@ import { copyClipboard } from "@/lib/copyClipboard";
 import { useState } from "react";
 import { generatePassword } from "@/lib/generatePassword";
 import { Textarea } from "@/components/ui/textarea";
+import { toast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
 
 export function FormAddElement() {
   const [showPassword, setShowPassword] = useState(false);
-
+  const router = useRouter();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -48,11 +51,30 @@ export function FormAddElement() {
     },
   });
 
-  // 2. Define a submit handler.
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.post("/api/items", values);
+      toast({ title: "Item created" });
+
+      form.reset({
+        typeElement: "",
+        isFavourite: false,
+        name: "",
+        directory: "",
+        username: "",
+        password: "",
+        urlWebsite: "",
+        notes: "",
+        userId: "ñlkjñl",
+      });
+
+      router.refresh();
+    } catch (error) {
+      toast({
+        title: "Something went wrong",
+        variant: "destructive",
+      });
+    }
   };
 
   const generateRandomPassword = () => {
